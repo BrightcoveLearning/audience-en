906be57d0801ca288c5147d80ff05081f3f68e6c
Bob Bailey <bbailey@brightcove.com>
2020-01-31 15:39:35 -0500

    update index page

--- _site/assets/js/feedback.js
+++ _site/assets/js/feedback.js
@@ -1,5 +1,5 @@
 var fby = fby || [];
-fby.push(['showTab', {id: '15071', position: 'right', color: 'rgb(4, 155, 179)'}]);
+fby.push(['showTab', {id: '', position: 'right', color: 'rgb(4, 155, 179)'}]);
 (function () {
     var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
     f.src = '//cdn.feedbackify.com/f.js';
