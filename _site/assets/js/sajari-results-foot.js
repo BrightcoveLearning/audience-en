0aafbcefdb4c204d34103e41c236d4bc17db1743
Bob Bailey <bbailey@brightcove.com>
2020-05-01 10:14:07 -0400

    update hubspot lead form doc

--- _site/assets/js/sajari-results-foot.js
+++ _site/assets/js/sajari-results-foot.js
@@ -13,6 +13,6 @@
       maxSuggestions: 5, // Maximum number of suggestions to show.
       results: {"showImages": false }, // Configure the results.
       values: {"q.override": true, "resultsPerPage": "10","q": getUrlParam("q")}, // Set default values.
-      tabFilters: {defaultTab:"On this site",tabs:[{title:"On this site",filter:"domain='audience.support.brightcove.com'"}, {title:"All Brightcove docs",filter:""}]}, // User selectable filters
+      tabFilters: {defaultTab:"On this site",tabs:[{title:"On this site",filter:"domain=''"}, {title:"All Brightcove docs",filter:""}]}, // User selectable filters
       styling: { theme: { colors: { brand: { primary: "#333" }}}}
   });
