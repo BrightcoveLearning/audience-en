2da35e35d2ca21d09bc16fd9543e33a8cc5847ea
Robert Crooks <rcrooks@brightcove.com>
2020-05-13 07:56:24 -0400

    general-update

--- _site/assets/js/sajari-results-foot.js
+++ _site/assets/js/sajari-results-foot.js
@@ -11,7 +11,7 @@
   } else {
     lang = 'en-us';
   }
-  site_domain = lang_prefix + '';
+  site_domain = lang_prefix + 'audience.support.brightcove.com';
   filter = "domain='" + site_domain +  "'";
   lang_filter = "lang='" + lang + "'";
   console.log('filter', filter);
